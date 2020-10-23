ec0abb42a8fb723cd73f7ffdb9766dd281244a2e
Marek Starzyk <stachu.ms9@gmail.com>
2020-10-23 13:49:56 +0200

    Add the sum of products

--- shopping_list.py
+++ shopping_list.py
@@ -2,8 +2,15 @@ shopping_list = {
     'piekarnia' : ['chleb', 'bułka', 'pączek'],
     'warzywniak' : ['marchew', 'seler', 'rukola']
 }
+number_off_products = 0
+
+
 for shop, products in shopping_list.items():
+    number_off_products += len(products)
     shop = shop.capitalize()
     for i in range(len(products)):
         products[i] = products[i].capitalize()
     print(f'Idę do {shop} i kupuję tam : {products}.')
+
+
+print(f'W sumie kupuję {number_off_products} produktów')
