d3ce8d5fda274d16e03f45b35f7c1c8033ef3cad
Marek Starzyk <stachu.ms9@gmail.com>
2020-10-23 13:32:50 +0200

    Add a for loop

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,5 @@ shopping_list = {
     'piekarnia' : ['chleb', 'bułka', 'pączek'],
     'warzywniak' : ['marchew', 'seler', 'rukola']
 }
+for shop, products in shopping_list.items():
+    print(f'Idę do {shop} i kupuję tam : {products}.')
